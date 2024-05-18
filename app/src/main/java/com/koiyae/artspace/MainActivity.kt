3d550a582cad65e408f395cd969c5699a36931b3
koiyae <bruno.ryan.bs@gmail.com>
2024-05-18 00:59:11 -0300

    1.1

--- app/src/main/java/com/koiyae/artspace/MainActivity.kt
+++ app/src/main/java/com/koiyae/artspace/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.koiyae.artspace
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Space
 import androidx.activity.ComponentActivity
-import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
@@ -17,49 +14,35 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ListItemDefaults.contentColor
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.focus.focusModifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -102,17 +85,16 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    @OptIn(ExperimentalComposeUiApi::class)
     @Composable
     fun MainScreen(modifier: Modifier = Modifier) {
         val firstArtwork = R.drawable.capture
         val secondArtwork = R.drawable.crucifixion
         val thirdArtwork = R.drawable.calling
 
-        var title by remember { mutableStateOf(R.string.capture) }
-        var year by remember { mutableStateOf(R.string.capture_year) }
-        var author by remember { mutableStateOf(R.string.capture_author) }
-        var currentArtwork by remember { mutableStateOf(firstArtwork) }
+        var title by remember { mutableIntStateOf(R.string.capture) }
+        var year by remember { mutableIntStateOf(R.string.capture_year) }
+        var author by remember { mutableIntStateOf(R.string.capture_author) }
+        var currentArtwork by remember { mutableIntStateOf(firstArtwork) }
 
         Column(
             modifier = modifier
